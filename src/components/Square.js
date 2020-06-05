31cb8c11a1c0026bee700a89c573a835260b601f
popluzhna18@gmail.com <popluzhna18@gmail.com>
2020-06-06 00:32:05 +0300

    Upd: add basic (fixes #1)

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style/Square.css'
+export default class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
