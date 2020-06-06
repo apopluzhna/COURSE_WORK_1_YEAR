2240e1d5c3e670c0892a6c554a4abef5b332a056
popluzhna18@gmail.com <popluzhna18@gmail.com>
2020-06-06 13:00:03 +0300

    Upd: add order of turns (fixes #6)

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,14 +5,18 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares : Array(9).fill(null)
+      squares : Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
     renderSquare(i) {
       return (
@@ -24,7 +28,7 @@ export default class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next turn: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
